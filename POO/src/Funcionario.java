469e8ab6ad13fdf18b2e7e05613bf7630af5ab4b
wisan <121894013+wisann@users.noreply.github.com>
2023-02-18 15:44:04 -0300

    project from college

---
+++ POO/src/Funcionario.java
@@ -0,0 +1,43 @@
+import javax.swing.JOptionPane;
+
+public class Funcionario {
+	
+	private String funcao;
+	private String admissao;
+	private String horaEntrada;
+	public String getFuncao() {
+		return funcao;
+	}
+	public void setFuncao(String funcao) {
+		this.funcao = funcao;
+	}
+	public String getAdmissao() {
+		return admissao;
+	}
+	public void setAdmissao(String admissao) {
+		this.admissao = admissao;
+	}
+	public String getHoraEntrada() {
+		return horaEntrada;
+	}
+	public void setHoraEntrada(String horaEntrada) {
+		this.horaEntrada = horaEntrada;
+	}
+	
+	public void insertDados() {
+		this.funcao = (JOptionPane.showInputDialog("Funçao : "));
+		this.admissao =(JOptionPane.showInputDialog("Admissao: "));
+		this.horaEntrada =(JOptionPane.showInputDialog("Hora de entrada: "));
+	
+	}
+	
+	public void MostraDados() {
+        
+        String text = "Dados do funcionario\n";
+		text += "\nFunção: " + this.getFuncao();
+		text += "\nData de admissao: " + this.getAdmissao();
+		text += "\nHora de entrada: " + this.getHoraEntrada();
+		JOptionPane.showMessageDialog(null, text);
+		}
+
+}
---
+++ POO/src/Main.java
@@ -0,0 +1,21 @@
+
+public class Main {
+	public static void main(String[] args) {
+		Pessoa pessoa1 = new Pessoa();
+		pessoa1.insertDados();
+		pessoa1.MostrarDados();
+		
+		Medico medico1 = new Medico();
+		medico1.insertDados();
+		medico1.MostrarDados();
+		
+		Paciente paciente1 = new Paciente();
+		paciente1.inserirDado();
+		paciente1.MostrarDado();
+		
+		Funcionario funcionario1 = new Funcionario();
+		funcionario1.insertDados();
+		funcionario1.MostraDados();
+	}
+
+}
