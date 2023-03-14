88d51ceda494a048a1d655942703a39054584d0e
wisan <121894013+wisann@users.noreply.github.com>
2023-03-14 10:01:25 -0300

    practicing While

--- While/src/Exercicio2.java
+++ While/src/Exercicio2.java
@@ -1,7 +1,7 @@
 //Escreva um programa para ler as coordenadas (X,Y) de uma quantidade indeterminada de pontos no sistema cartesiano. Para cada ponto escrever o quadrante a que ele pertence. O algoritmo será encerrado quando pelo menos uma de duas coordenadas for NULA (nesta situação sem escrever mensagem alguma).
 import java.util.Scanner;
 
-public class Exercicio2 {
+public class Exercicio2 { 
 
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
